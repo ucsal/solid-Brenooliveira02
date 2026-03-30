10dfdf6c9021e4abfb13c97b68b4dd07f778b6c3
João Breno <oliveirabreno847@gmail.com>
2026-03-29 21:26:20 -0300

    Adição do método calcularNota()

--- src/main/java/br/com/ucsal/olimpiadas/Tentativa.java
+++ src/main/java/br/com/ucsal/olimpiadas/Tentativa.java
@@ -4,38 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tentativa {
+
 	private long id;
 	private long participanteId;
 	private long provaId;
-
 	private final List<Resposta> respostas = new ArrayList<>();
 
-	public long getId() {
-		return id;
-	}
+	public long getId()                     { return id; }
+	public void setId(long id)              { this.id = id; }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+	public long getParticipanteId()         { return participanteId; }
+	public void setParticipanteId(long pid) { this.participanteId = pid; }
 
-	public long getParticipanteId() {
-		return participanteId;
-	}
+	public long getProvaId()                { return provaId; }
+	public void setProvaId(long provaId)    { this.provaId = provaId; }
 
-	public void setParticipanteId(long participanteId) {
-		this.participanteId = participanteId;
-	}
+	public List<Resposta> getRespostas()    { return respostas; }
 
-	public long getProvaId() {
-		return provaId;
+	public int calcularNota() {
+		int acertos = 0;
+		for (Resposta r : respostas)
+			if (r.isCorreta()) acertos++;
+		return acertos;
 	}
-
-	public void setProvaId(long provaId) {
-		this.provaId = provaId;
-	}
-
-	public List<Resposta> getRespostas() {
-		return respostas;
-	}
-
 }
